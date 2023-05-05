8e268e6f8e1afdbbff6658178557cab8b1bd76a3
Zara <zarabucknor@yahoo.co.uk>
2023-05-05 21:37:53 +0100

    loading data method

--- directory.rb
+++ directory.rb
@@ -1,5 +1,14 @@
 @students = [] 
 
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
 def save_students
   file = File.open("students.csv", "w")
   @students.each do |student|
@@ -34,6 +43,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit" # 9 because we'll be adding more items  
 end
 
